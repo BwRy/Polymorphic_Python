ddc9da91b29ecf8d9a5b47196a4235522993be31
Jacob Doyelle Yates <blmvxer@gmail.com>
2015-02-02 15:05:44 -0500

    Create Poly.py

---
+++ Poly.py
@@ -0,0 +1,53 @@
+import random, os
+from string import *
+qedBQUMJ = "#qedBQUMJ#"
+aIgRMmVDHlBS = "#aIgRMmVDHlBS#"
+CrMLiKGQfzEFvx = 0
+YWKsycFA = open(__file__, 'r')
+VxNuGbalQFndP = ['qedBQUMJ', 'aIgRMmVDHlBS', 'IwxXGPFAbqWd', 'VxNuGbalQFndP', 'CrMLiKGQfzEFvx', 'hwBlMOXJSZka', 'QDGZoYKxywp', 'oHWhsXIJyb', 'GrNdyQfuzwoWL', 'YWKsycFA', 'gqzYMGcPx', 'uSAyplDIPBcTE', 'vhpRqZMJxBSIy', 'AqcJMtyh', 'eDyamBTq', 'NLmbacYehrpP', 'nTIHzjYsrUDiXq', 'XOqCjYkKEhWPZ', 'eInvGtzHdlTi',]
+uSAyplDIPBcTE = len(VxNuGbalQFndP)
+uSAyplDIPBcTE = uSAyplDIPBcTE - 1
+vhpRqZMJxBSIy = []
+while CrMLiKGQfzEFvx <= uSAyplDIPBcTE:
+    hwBlMOXJSZka = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    GrNdyQfuzwoWL = random.randrange(8, 17)
+    QDGZoYKxywp = GrNdyQfuzwoWL
+    oHWhsXIJyb = "".join(random.sample(hwBlMOXJSZka, QDGZoYKxywp))
+    vhpRqZMJxBSIy.append(oHWhsXIJyb)
+    CrMLiKGQfzEFvx = CrMLiKGQfzEFvx + 1
+#aIgRMmVDHlBS##qedBQUMJ##IwxXGPFAbqWd#
+IwxXGPFAbqWd = "#IwxXGPFAbqWd#" 
+import re
+IwxXGPFAbqWd
+def eDyamBTq(eInvGtzHdlTi, wordDic):
+	return re.sub('|'.join(re.escape(key) for key in eInvGtzHdlTi.keys()), lambda k: eInvGtzHdlTi[k.group(0)], wordDic)
+#aIgRMmVDHlBS##qedBQUMJ##IwxXGPFAbqWd#
+NLmbacYehrpP = YWKsycFA.read() 
+#aIgRMmVDHlBS##qedBQUMJ##IwxXGPFAbqWd#
+#aIgRMmVDHlBS##qedBQUMJ##IwxXGPFAbqWd#
+wordDic = {
+VxNuGbalQFndP[0]: vhpRqZMJxBSIy[0],
+VxNuGbalQFndP[1]: vhpRqZMJxBSIy[1],
+VxNuGbalQFndP[2]: vhpRqZMJxBSIy[2],
+VxNuGbalQFndP[3]: vhpRqZMJxBSIy[3],
+VxNuGbalQFndP[4]: vhpRqZMJxBSIy[4],
+VxNuGbalQFndP[5]: vhpRqZMJxBSIy[5],
+VxNuGbalQFndP[6]: vhpRqZMJxBSIy[6],
+VxNuGbalQFndP[7]: vhpRqZMJxBSIy[7],
+VxNuGbalQFndP[8]: vhpRqZMJxBSIy[8],
+VxNuGbalQFndP[9]: vhpRqZMJxBSIy[9],
+VxNuGbalQFndP[10]: vhpRqZMJxBSIy[10],
+VxNuGbalQFndP[11]: vhpRqZMJxBSIy[11],
+VxNuGbalQFndP[12]: vhpRqZMJxBSIy[12],
+VxNuGbalQFndP[13]: vhpRqZMJxBSIy[13],
+VxNuGbalQFndP[14]: vhpRqZMJxBSIy[14],
+VxNuGbalQFndP[15]: vhpRqZMJxBSIy[15],
+VxNuGbalQFndP[16]: vhpRqZMJxBSIy[16],
+VxNuGbalQFndP[17]: vhpRqZMJxBSIy[17],
+VxNuGbalQFndP[18]: vhpRqZMJxBSIy[18]}
+#aIgRMmVDHlBS##qedBQUMJ##IwxXGPFAbqWd#
+XOqCjYkKEhWPZ = eDyamBTq(wordDic, NLmbacYehrpP)
+nTIHzjYsrUDiXq = open(__file__, 'w+')
+nTIHzjYsrUDiXq.write(XOqCjYkKEhWPZ)
+nTIHzjYsrUDiXq.close()
+#aIgRMmVDHlBS##qedBQUMJ##IwxXGPFAbqWd#
