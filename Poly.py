73b3fbdd7798f4ea2d9708dddf41f38a052ba6c3
Jacob Doyelle Yates <blmvxer@gmail.com>
2015-07-29 12:45:03 -0400

    Updated to work with Python2 and Python3

--- Poly.py
+++ Poly.py
@@ -1,25 +1,25 @@
 import random, os, re
 from string import *
-HTcqAfulpgvsw = 0
-AHWQrzZVlIGkth = 1
-SuvHEgbmDadUrBF = __file__
-GWetjmpc = int(HTcqAfulpgvsw)
-eLdwQjq = open(SuvHEgbmDadUrBF, 'r')
-qVTKZsUlNYMykGPw = ['oaDgwrBPEypfcbNdi', 'meFUvVWLNcSXMtQjKRz', 'HTcqAfulpgvsw', 'SuvHEgbmDadUrBF', 'AHWQrzZVlIGkth', 'qVTKZsUlNYMykGPw', 'GWetjmpc', 'ueImoqXZsfPU', 'uearpCsgHPjZby', 'lpigVLR', 'fWpzDKcZgUhQTFHrkw', 'eLdwQjq', 'fKdZwOIU', 'rtwFqCafshpgyz', 'JsNbwXzMxtmE', 'KHUgVFAmOwJdbpE', 'DhtnWaLFueroXsdMmkH', 'bBCMjrSqEhLGdQpezlT', 'dwHNMieAruxnSTqQbJ', 'HghTQJZqYIBloMWiN', 'DucdqTNhvMskG',]
-rtwFqCafshpgyz = len(qVTKZsUlNYMykGPw)
-rtwFqCafshpgyz = rtwFqCafshpgyz - int(AHWQrzZVlIGkth)
-JsNbwXzMxtmE = []
-while GWetjmpc <= rtwFqCafshpgyz:
-    ueImoqXZsfPU = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    fWpzDKcZgUhQTFHrkw = random.randrange(7, 20)
-    uearpCsgHPjZby = fWpzDKcZgUhQTFHrkw
-    lpigVLR = "".join(random.sample(ueImoqXZsfPU, uearpCsgHPjZby))
-    JsNbwXzMxtmE.append(lpigVLR)
-    GWetjmpc = GWetjmpc + int(AHWQrzZVlIGkth)
-def DhtnWaLFueroXsdMmkH(DucdqTNhvMskG, meFUvVWLNcSXMtQjKRz):
-	return re.sub('|'.join(re.escape(key) for key in DucdqTNhvMskG.keys()), lambda k: DucdqTNhvMskG[k.group(int(HTcqAfulpgvsw))], meFUvVWLNcSXMtQjKRz)
-bBCMjrSqEhLGdQpezlT = eLdwQjq.read()
-meFUvVWLNcSXMtQjKRz = {qVTKZsUlNYMykGPw[0]: JsNbwXzMxtmE[0], qVTKZsUlNYMykGPw[1]: JsNbwXzMxtmE[1], qVTKZsUlNYMykGPw[2]: JsNbwXzMxtmE[2], qVTKZsUlNYMykGPw[3]: JsNbwXzMxtmE[3], qVTKZsUlNYMykGPw[4]: JsNbwXzMxtmE[4], qVTKZsUlNYMykGPw[5]: JsNbwXzMxtmE[5], qVTKZsUlNYMykGPw[6]: JsNbwXzMxtmE[6], qVTKZsUlNYMykGPw[7]: JsNbwXzMxtmE[7], qVTKZsUlNYMykGPw[8]: JsNbwXzMxtmE[8], qVTKZsUlNYMykGPw[9]: JsNbwXzMxtmE[9], qVTKZsUlNYMykGPw[10]: JsNbwXzMxtmE[10], qVTKZsUlNYMykGPw[11]: JsNbwXzMxtmE[11], qVTKZsUlNYMykGPw[12]: JsNbwXzMxtmE[12], qVTKZsUlNYMykGPw[13]: JsNbwXzMxtmE[13], qVTKZsUlNYMykGPw[14]: JsNbwXzMxtmE[14], qVTKZsUlNYMykGPw[15]: JsNbwXzMxtmE[15], qVTKZsUlNYMykGPw[16]: JsNbwXzMxtmE[16], qVTKZsUlNYMykGPw[17]: JsNbwXzMxtmE[17], qVTKZsUlNYMykGPw[18]: JsNbwXzMxtmE[18], qVTKZsUlNYMykGPw[19]: JsNbwXzMxtmE[19]}
-HghTQJZqYIBloMWiN = DhtnWaLFueroXsdMmkH(meFUvVWLNcSXMtQjKRz, bBCMjrSqEhLGdQpezlT)
-dwHNMieAruxnSTqQbJ = open(SuvHEgbmDadUrBF, 'w+')
-dwHNMieAruxnSTqQbJ.write(HghTQJZqYIBloMWiN)
+uKhrsEyxXqTvHFGcpz = 0
+ntdGefTJLRqsaozHmIO = 1
+BwSkHTELsOxt = __file__
+YCeDMbtGg = int(uKhrsEyxXqTvHFGcpz)
+SlongaNVYyA = open(BwSkHTELsOxt, 'r')
+NUdWKflzyHwnu = ['ExoyhSZGtVDPbqknQ', 'wdPOKGUxqCEzejkWhIp', 'uKhrsEyxXqTvHFGcpz', 'BwSkHTELsOxt', 'ntdGefTJLRqsaozHmIO', 'NUdWKflzyHwnu', 'YCeDMbtGg', 'wDopTIASYECclgHzU', 'RJ$
+WhfiHByKdlJUrwzq = len(NUdWKflzyHwnu)
+WhfiHByKdlJUrwzq = WhfiHByKdlJUrwzq - int(ntdGefTJLRqsaozHmIO)
+zauGieXxjJCLqc = []
+while YCeDMbtGg <= WhfiHByKdlJUrwzq:
+    wDopTIASYECclgHzU = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    fhPtvaUz = random.randrange(7, 20)
+    RJeWsxoCy = fhPtvaUz
+    eqaLhApgkmdur = "".join(random.sample(wDopTIASYECclgHzU, RJeWsxoCy))
+    zauGieXxjJCLqc.append(eqaLhApgkmdur)
+    YCeDMbtGg = YCeDMbtGg + int(ntdGefTJLRqsaozHmIO)
+def yzFYLAwtnUbGPfWv(DucdqTNhvMskG, wdPOKGUxqCEzejkWhIp):
+        return re.sub('|'.join(re.escape(key) for key in list(DucdqTNhvMskG.keys())), lambda k: DucdqTNhvMskG[k.group(int(uKhrsEyxXqTvHFGcpz))], wdPOKGUxqCEzejkWhIp)
+hGXimDzI = SlongaNVYyA.read()
+wdPOKGUxqCEzejkWhIp = {NUdWKflzyHwnu[0]: zauGieXxjJCLqc[0], NUdWKflzyHwnu[1]: zauGieXxjJCLqc[1], NUdWKflzyHwnu[2]: zauGieXxjJCLqc[2], NUdWKflzyHwnu[3]: zauGieXxjJCLqc[3], NUdWK$
+YhqdKRSDA = yzFYLAwtnUbGPfWv(wdPOKGUxqCEzejkWhIp, hGXimDzI)
+BNlUyCoHMekusQEjG = open(BwSkHTELsOxt, 'w+')
+BNlUyCoHMekusQEjG.write(YhqdKRSDA)
